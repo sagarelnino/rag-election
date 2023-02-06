298c82c4a1c9533c1b6421e96c8db2e6368a050e
Shuvashish Paul Sagar <shuvashishpaul64@gmail.com>
2023-02-07 00:25:41 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,16 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header"></div>
-
+                    <div class="card-header">
+                        SUMMARY
+                    </div>
                     <div class="card-body">
-
+                        <ul>
+                            <li>Elections:</li>
+                            <li>Candidates:</li>
+                            <li>Voters:</li>
+                            <li>Waiting for approval:</li>
+                        </ul>
                     </div>
                 </div>
             </div>
