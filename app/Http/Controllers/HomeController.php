603973bbe1d1c32ae2a3700832527d61c3de789e
Shuvashish Paul Sagar <shuvashishpaul64@gmail.com>
2023-02-07 00:34:46 +0900

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,18 +26,18 @@ class HomeController extends Controller
     public function index()
     {
         if (auth()->user()->user_type == User::USER_TYPE_ADMIN) {
-            return view('home');
+            $elections = Election::where('is_active', true)->get();
+            $admins = User::where('is_approved', true)->where('user_type', User::USER_TYPE_ADMIN)->count();
+            $voters = User::where('is_approved', true)->where('user_type', User::USER_TYPE_VOTER)->count();
+            $approvals = User::where('is_approved', false)->where('user_type', User::USER_TYPE_VOTER)->count();
+            return view('home', ['elections' => $elections, 'admins' => $admins, 'voters' => $voters, 'approvals' => $approvals]);
         } elseif (auth()->user()->user_type == User::USER_TYPE_VOTER) {
             $query = User::query();
             $query->select('users.id', 'users.name', 'users.email', 'users.is_approved', 'user_details.hall', 'user_details.department', 'user_details.address', 'user_details.thumb', 'user_details.home_district', 'user_details.facebook_id');
             $query->join('user_details', 'users.id', '=', 'user_details.user_id');
             $query->where('users.id', '=', auth()->id());
             $user = $query->first();
-            $elections = Election::where('is_active', true)->get();
-
-            $voters = User::where('is_approved', true)->count();
-            $approvals = User::where('is_approved', false)->count();
-            return view('users.user_dashboard', ['user' => $user, 'elections' => $elections, 'voters' => $voters, 'approvals' => $approvals]);
+            return view('users.user_dashboard', ['user' => $user]);
         }
     }
 }
