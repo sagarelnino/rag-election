2ad8aef63761d57a26d1c73fe20c8b7a154efd73
Shuvashish Paul Sagar <shuvashishpaul64@gmail.com>
2023-02-09 01:22:57 +0900

    Update approve.blade.php

--- resources/views/email/approve.blade.php
+++ resources/views/email/approve.blade.php
@@ -2,9 +2,21 @@
 
 @if ($is_approved == 'approve')
     Congratulations!!!<br />
-    Your request to RAG43 Election has been approved. You can login to explore.
-
+    Your request to RAG43 Election has been approved. <br />
+    You can login to explore.
     <a href="{{ route('login') }}">Login</a>
+    <br />
+    <h3>Notice</h3>
+    <li>You have to cast your vote within Bangladesh time 9AM-5PM(February 10, 2023)</li>
+    <li>After login you will see a green Elections button. Click on the button</li>
+    <li>You will see a election list. Click on the VOTE button</li>
+    <li>You will be redirected to the election details page</li>
+    <li>Here you can see the king & queen options</li>
+    <li>Choose one king and one queen and submit your vote</li>
+    <li>That's it</li>
+    <br />
+    <h4>হৃদয় সুতোয় আখর টেনে <br />
+        বন্ধুই জানে বন্ধুর মানে৷</h4>
 @else
     Sorry!!!<br />
     Unfortunately we are unable to approve your request to join the RAG43 Election.
